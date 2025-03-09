991da93192905aa078d2dc31cf88b82d88e80acb
Thaetami <tatanghaetami.97@gmail.com>
2025-03-09 14:56:13 +0700

    update

--- next.config.mjs
+++ next.config.mjs
@@ -9,12 +9,36 @@ export default withPWA({
   skipWaiting: true,
   runtimeCaching: [
     {
+      urlPattern: ({ request }) => request.destination === "document",
+      handler: "NetworkFirst",
+      options: {
+        cacheName: "pages",
+        expiration: {
+          maxEntries: 10,
+          maxAgeSeconds: 24 * 60 * 60,
+        },
+        networkTimeoutSeconds: 3,
+      },
+    },
+    {
       urlPattern: /\.(?:png|jpg|jpeg|svg|gif)$/,
       handler: "CacheFirst",
       options: {
         cacheName: "icon",
         expiration: {
           maxEntries: 10,
+          maxAgeSeconds: 30 * 24 * 60 * 60,
+        },
+      },
+    },
+    {
+      urlPattern: /\.(?:js|css)$/,
+      handler: "CacheFirst",
+      options: {
+        cacheName: "static-resources",
+        expiration: {
+          maxEntries: 50,
+          maxAgeSeconds: 30 * 24 * 60 * 60,
         },
       },
     },
