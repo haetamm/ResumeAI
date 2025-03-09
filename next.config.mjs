2f3ec7a9a6e70baea09e372947082c18f4800577
Thaetami <tatanghaetami.97@gmail.com>
2025-03-09 14:46:46 +0700

    update

--- next.config.mjs
+++ next.config.mjs
@@ -7,4 +7,16 @@ export default withPWA({
   dest: "public",
   register: true,
   skipWaiting: true,
+  runtimeCaching: [
+    {
+      urlPattern: /\.(?:png|jpg|jpeg|svg|gif)$/,
+      handler: "CacheFirst",
+      options: {
+        cacheName: "icon",
+        expiration: {
+          maxEntries: 10,
+        },
+      },
+    },
+  ],
 })(nextConfig);
