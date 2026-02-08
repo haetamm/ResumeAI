90709a6f16d00d00260649b789830804c6425441
Tatang Haetami <tatanghaetami.97@gmail.com>
2026-02-08 22:28:51 +0700

    update

--- lib/actions/gemini.actions.ts
+++ lib/actions/gemini.actions.ts
@@ -2,14 +2,12 @@
 
 import {
   GoogleGenerativeAI,
-  HarmCategory,
-  HarmBlockThreshold,
 } from "@google/generative-ai";
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY!);
 
 const model = genAI.getGenerativeModel({
-  model: "gemini-2.0-flash",
+  model: "gemini-2.5-flash-lite",
 });
 
 const generationConfig = {
